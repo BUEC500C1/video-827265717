454b400ba94e717664cf0bac0b5dc41ba8305268
song geng <827265717@qq.com>
2020-02-23 12:06:40 -0500

    Update image2video.py

--- image2video.py
+++ image2video.py
@@ -5,12 +5,9 @@ import numpy as np
 def image_to_video(keyword):
 	path = 'img/'
 	filelist = os.listdir(path)
-
-	fps = 1/3 # three seconds per pic
+	fps = 1/3
 	size = (1200, 630)
-
 	video = cv2.VideoWriter(keyword + ".avi", cv2.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
-
 	for item in filelist:
 		if item.endswith('.png'): 
 			item = path + item
