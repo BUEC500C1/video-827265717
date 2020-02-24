fa57420f4b635ffbe97be5281c900b62ff19f658
song geng <827265717@qq.com>
2020-02-24 09:23:44 -0500

    Update test_systemQueue.py

--- test_systemQueue.py
+++ test_systemQueue.py
@@ -1,4 +1,4 @@
-from tweepy import tweepy_get
+from getTweepy import tweepy_get
 from image2video import image_to_video
 from systemQueue import queue_1
 import multiprocessing
@@ -12,7 +12,7 @@ def test_twitter_get():
   for key in keyNames:
     pass
 
-def test_image2video():
+def test_image_to_video():
   keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
   for key in keyNames:
     pass
