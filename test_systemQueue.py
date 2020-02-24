363c6b15d53cbb1be5b570e61644b1ec76e9d530
song geng <827265717@qq.com>
2020-02-24 09:15:56 -0500

    Update test_systemQueue.py

--- test_systemQueue.py
+++ test_systemQueue.py
@@ -1,3 +1,22 @@
-import pytest 
-def test_something():
+from tweepy_get import tweepy_get
+from image2video import image_to_video
+from queue_sys import queue_1
+import multiprocessing
+import time
+import threading
+import queue
+import os
+
+def test_twitter_get():
+  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+  for key in keyNames:
     pass
+
+def test_image2video():
+  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+  for key in keyNames:
+    pass
+    
+def test_queue():
+  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+  number_thread = 4
